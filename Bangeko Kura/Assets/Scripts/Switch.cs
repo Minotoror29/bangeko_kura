fd35cb7acbcd99287e26b5baa8ec7e2226ec0e6e
Minotoror29 <matthieu.boissel@laposte.net>
2024-03-27 15:01:11 +0100

    Switch Platforms Transitions

--- Bangeko Kura/Assets/Scripts/Switch.cs
+++ Bangeko Kura/Assets/Scripts/Switch.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SwitchState { AState, BState, SwitchingToA, SwitchingToB }
+
 public class Switch : MonoBehaviour
 {
     [SerializeField] private List<SwitchPlatform> platformsA;
     [SerializeField] private List<SwitchPlatform> platformsB;
 
-    private bool _switchState = true;
+    [SerializeField] private float switchingTime = 2f;
+    private float _switchingTimer = 0f;
+
+    private SwitchState _currentState;
 
     private void Start()
     {
@@ -20,39 +25,68 @@ public class Switch : MonoBehaviour
         {
             platform.gameObject.SetActive(false);
         }
+
+        _currentState = SwitchState.AState;
     }
 
     public void Activate()
     {
-        SwitchState();
+        ChangeState();
     }
 
-    private void SwitchState()
+    private void ChangeState()
     {
-        _switchState = !_switchState;
+        //_switchingTimer = switchingTime;
 
-        if (_switchState)
+        if (_currentState == SwitchState.AState)
         {
-            foreach (SwitchPlatform platform in platformsA)
-            {
-                platform.gameObject.SetActive(true);
-            }
-
-            foreach (SwitchPlatform platform in platformsB)
-            {
-                platform.gameObject.SetActive(false);
-            }
-        } else
+            _currentState = SwitchState.SwitchingToB;
+            StartCoroutine(EnterPlatforms(SwitchState.BState, platformsB, platformsA));
+        } else if (_currentState == SwitchState.BState)
         {
-            foreach (SwitchPlatform platform in platformsB)
-            {
-                platform.gameObject.SetActive(true);
-            }
-
-            foreach (SwitchPlatform platform in platformsA)
-            {
-                platform.gameObject.SetActive(false);
-            }
+            _currentState = SwitchState.SwitchingToA;
+            StartCoroutine(EnterPlatforms(SwitchState.AState, platformsA, platformsB));
         }
+    }
+
+    private IEnumerator EnterPlatforms(SwitchState stateToSwitchTo, List<SwitchPlatform> platformsToEnter, List<SwitchPlatform> platformsToExit)
+    {
+        foreach (SwitchPlatform platform in platformsToEnter)
+        {
+            platform.Enter();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine(ExitPlatforms(stateToSwitchTo, platformsToExit));
+    }
+
+    private IEnumerator ExitPlatforms(SwitchState stateToSwitchTo, List<SwitchPlatform> platforms)
+    {
+        foreach (SwitchPlatform platform in platforms)
+        {
+            platform.Exit();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        _currentState = stateToSwitchTo;
+    }
+
+    private void Update()
+    {
+        //if (_switchingTimer > 0f)
+        //{
+        //    _switchingTimer -= Time.deltaTime;
+        //} else
+        //{
+        //    if (_currentState == SwitchState.SwitchingToA)
+        //    {
+        //        _currentState = SwitchState.AState;
+        //    } else if (_currentState == SwitchState.SwitchingToB)
+        //    {
+        //        _currentState = SwitchState.BState;
+        //    }
+        //}
     }
 }
--- Bangeko Kura/Assets/Scripts/SwitchPlatform.cs
+++ Bangeko Kura/Assets/Scripts/SwitchPlatform.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SwitchPlatformState { Entering, Exiting, Idle }
+
 public class SwitchPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator animator;
+    [SerializeField] private Collider2D coll;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private SwitchPlatformState _currentState = SwitchPlatformState.Idle;
+
+    [SerializeField] private float animationTime = 0.5f;
+    private float _animationTimer = 0f;
+
+    public void Enter()
     {
-        
+        //_currentState = SwitchPlatformState.Entering;
+        //_animationTimer = animationTime;
+
+        gameObject.SetActive(true);
+        coll.enabled = true;
+        animator.CrossFade("Enter", 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        //_currentState = SwitchPlatformState.Exiting;
+        //_animationTimer = animationTime;
+
+        coll.enabled = false;
+        animator.CrossFade("Exit", 0f);
+    }
+
+    private void Update()
+    {
+        //if (_currentState != SwitchPlatformState.Idle)
+        //{
+        //    if (_animationTimer > 0f)
+        //    {
+        //        _animationTimer -= Time.deltaTime;
+        //    } else
+        //    {
+        //        _currentState = SwitchPlatformState.Idle;
+        //    }
+        //}
     }
 }
