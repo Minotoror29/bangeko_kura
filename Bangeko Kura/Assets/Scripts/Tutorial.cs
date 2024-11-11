19073bcf169671d56293ec5c484733bddbedcf29
Minotoror29 <matthieu.boissel@laposte.net>
2024-11-11 22:46:35 +0100

    Sword Animation Fixes

--- Bangeko Kura/Assets/Scripts/Tutorial.cs
+++ Bangeko Kura/Assets/Scripts/Tutorial.cs
@@ -10,6 +10,8 @@ public class Tutorial : MonoBehaviour
     [SerializeField] private GameObject tutorialAnimation;
     [SerializeField] private List<SpriteRenderer> spriteRenderers;
     [SerializeField] private float fadeTime = 1f;
+    [SerializeField] private CinemachineVirtualCamera tutorialCam;
+    [SerializeField] private CinemachineVirtualCamera levelCam;
 
     private TutorialState _currentState = TutorialState.Inactive;
     private float _fadeTimer = 0f;
@@ -18,6 +20,7 @@ public class Tutorial : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            CameraManager.Instance.ChangeCamera(tutorialCam);
             _currentState = TutorialState.Entering;
         }
     }
@@ -26,6 +29,7 @@ public class Tutorial : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            CameraManager.Instance.ChangeCamera(levelCam);
             _currentState = TutorialState.Exiting;
         }
     }
